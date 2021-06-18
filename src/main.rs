9a15a03103078785db596b917ae63efc6df9af11
Charlie Little <cwlittle@utexas.edu>
2021-06-18 00:50:41 -0500

    Capture output from multiple toolkit test runs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::process::Command;
+
+use std::str;
+
+fn run_tests(toolkit: &str) -> String {
+    let output = Command::new("rustup")
+        .arg("run")
+        .arg(toolkit)
+        .arg("cargo")
+        .arg("test")
+        .arg("--")
+        .arg("--show-output")
+        .output()
+        .expect("Error running command");
+
+    match str::from_utf8(&output.stdout) {
+        Ok(v) => v.to_string(),
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let toolkits = [
+        "nightly-2021-06-03-x86_64-apple-darwin",
+        "nightly-x86_64-apple-darwin",
+    ];
+
+    for kit in toolkits.iter() {
+        println!("{}", run_tests(kit));
+    }
 }
