55e59bcf758bf25a44ca4d1fbb98cdc929151b88
Charlie Little <cwlittle@utexas.edu>
2021-06-18 17:39:42 -0500

    Add terminal output parsing

--- src/main.rs
+++ src/main.rs
@@ -19,13 +19,40 @@ fn run_tests(toolkit: &str) -> String {
     }
 }
 
+fn parse_test_output(output: &str) -> Vec<String> {
+    let lines: Vec<String> = output.split("\n").map(|x| x.to_string()).collect();
+
+    lines
+}
+
 fn main() {
     let toolkits = [
         "nightly-2021-06-03-x86_64-apple-darwin",
         "nightly-x86_64-apple-darwin",
     ];
 
-    for kit in toolkits.iter() {
-        println!("{}", run_tests(kit));
+    for kit in toolkits {
+        let lines = parse_test_output(&run_tests(kit));
+        println!("toolkit: {}", kit);
+        println!("{:?}", lines);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_single_test() {
+        let input = "test tests::test_1 ... ok";
+        let expected = vec!["test tests::test_1 ... ok"];
+        assert_eq!(parse_test_output(input), expected);
+    }
+
+    #[test]
+    fn parse_double_test() {
+        let input = "Hello\nWorld";
+        let expected = vec!["Hello", "World"];
+        assert_eq!(parse_test_output(input), expected);
     }
 }
