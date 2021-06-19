018c0c5ba9e4153c70cde32f2a438c0a3f40373f
Charlie Little <cwlittle@utexas.edu>
2021-06-19 18:14:14 -0500

    Add test output extraction

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use regex::Regex;
 use std::collections::BTreeSet;
 use std::process::Command;
 use std::str;
+use string_builder::Builder;
 
 fn run_tests(toolkit: &str) -> String {
     let output = Command::new("rustup")
@@ -51,6 +52,30 @@ fn get_test_result(test_name: &String, lines: &Vec<String>) -> bool {
     panic!("You shouldn't be here");
 }
 
+fn get_test_output(test_name: &String, lines: &Vec<String>) -> String {
+    let mut start: usize = 0;
+    let mut end: usize = lines.len() - 1;
+
+    let mut builder = Builder::default();
+    builder.append("---- ");
+    builder.append(test_name.clone());
+    builder.append(" stdout ----");
+    let start_string = builder.string().unwrap();
+    let start_re = Regex::new(&start_string).unwrap();
+    let next_test_re = Regex::new(r"---- [a-zA-Z_0-9]+::[a-zA-Z_0-9]+ stdout ----").unwrap();
+    let failure_re = Regex::new(r"failures:").unwrap();
+
+    for (i, line) in lines.iter().enumerate() {
+        if start_re.is_match(line) {
+            start = i + 1;
+        } else if next_test_re.is_match(line) || failure_re.is_match(line) {
+            end = i - 1;
+            break;
+        }
+    }
+    lines[start..=end].concat()
+}
+
 fn main() {
     let toolkits = [
         "nightly-2021-06-03-x86_64-apple-darwin",
@@ -61,8 +86,8 @@ fn main() {
         let lines = parse_test_output(&run_tests(kit));
         let unique_test_names = get_test_names(&lines);
         for test in unique_test_names.iter() {
-            let test_output = get_test_result(&test, &lines);
-            println!("{}: {}", test, test_output);
+            let test_result = get_test_result(&test, &lines);
+            let test_output = get_test_output(&test, &lines);
         }
     }
 }
@@ -91,5 +116,43 @@ mod tests {
         let mut expected = BTreeSet::new();
         expected.insert(String::from("tests::test_1"));
         assert_eq!(get_test_names(&input), expected);
+    }
+
+    #[test]
+    fn extract_test_output_break_end() {
+        let test_name = String::from("tests::test_1");
+        let lines = vec![
+            String::from("what"),
+            String::from("---- tests::test_1 stdout ----"),
+            String::from("Hello "),
+            String::from("World"),
+        ];
+        assert_eq!(get_test_output(&test_name, &lines), "Hello World");
+    }
+
+    #[test]
+    fn extract_test_output_break_next_test() {
+        let test_name = String::from("tests::test_1");
+        let lines = vec![
+            String::from("what"),
+            String::from("---- tests::test_1 stdout ----"),
+            String::from("Hello "),
+            String::from("World"),
+            String::from("---- tests::test_2 stdout ----"),
+        ];
+        assert_eq!(get_test_output(&test_name, &lines), "Hello World");
+    }
+
+    #[test]
+    fn extract_test_output_break_failures() {
+        let test_name = String::from("tests::test_1");
+        let lines = vec![
+            String::from("what"),
+            String::from("---- tests::test_1 stdout ----"),
+            String::from("Hello "),
+            String::from("World"),
+            String::from("failures:"),
+        ];
+        assert_eq!(get_test_output(&test_name, &lines), "Hello World");
     }
 }
