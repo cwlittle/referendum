07086104c3e462badbb7ddaa992e3b66fe653836
Charlie Little <cwlittle@utexas.edu>
2021-06-19 21:20:46 -0500

    Add hash and test structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use fasthash::sea;
 use regex::Regex;
 use std::collections::BTreeSet;
 use std::process::Command;
@@ -12,6 +13,7 @@ fn run_tests(toolkit: &str) -> String {
         .arg("test")
         .arg("--")
         .arg("--show-output")
+        .arg("--test-threads=1")
         .output()
         .expect("Error running command");
 
@@ -53,6 +55,7 @@ fn get_test_result(test_name: &String, lines: &Vec<String>) -> bool {
 }
 
 fn get_test_output(test_name: &String, lines: &Vec<String>) -> String {
+    //this doesn't work if there are multiple success and failures in the group
     let mut start: usize = 0;
     let mut end: usize = lines.len() - 1;
 
@@ -64,13 +67,23 @@ fn get_test_output(test_name: &String, lines: &Vec<String>) -> String {
     let start_re = Regex::new(&start_string).unwrap();
     let next_test_re = Regex::new(r"---- [a-zA-Z_0-9]+::[a-zA-Z_0-9]+ stdout ----").unwrap();
     let failure_re = Regex::new(r"failures:").unwrap();
+    let success_re = Regex::new(r"successes:").unwrap();
 
+    let mut start_found = false;
     for (i, line) in lines.iter().enumerate() {
         if start_re.is_match(line) {
+            start_found = true;
             start = i + 1;
-        } else if next_test_re.is_match(line) || failure_re.is_match(line) {
-            end = i - 1;
-            break;
+        } else if next_test_re.is_match(line)
+            || failure_re.is_match(line)
+            || success_re.is_match(line)
+        {
+            if start_found {
+                if i - 1 > start {
+                    end = i - 1;
+                    break;
+                }
+            }
         }
     }
     lines[start..=end].concat()
@@ -82,12 +95,31 @@ fn main() {
         "nightly-x86_64-apple-darwin",
     ];
 
+    #[derive(Debug)]
+    struct Test {
+        name: String,
+        result: bool,
+        output: String,
+        hash: u64,
+        matches_consensus: bool,
+    }
+
+    let mut tests: Vec<Test> = Vec::new();
     for kit in toolkits {
         let lines = parse_test_output(&run_tests(kit));
         let unique_test_names = get_test_names(&lines);
         for test in unique_test_names.iter() {
             let test_result = get_test_result(&test, &lines);
             let test_output = get_test_output(&test, &lines);
+            let output_hash = sea::hash64(&test_output.as_bytes());
+            let output_obj = Test {
+                name: test.clone(),
+                result: test_result,
+                output: test_output,
+                hash: output_hash,
+                matches_consensus: false,
+            };
+            tests.push(output_obj);
         }
     }
 }
@@ -154,5 +186,56 @@ mod tests {
             String::from("failures:"),
         ];
         assert_eq!(get_test_output(&test_name, &lines), "Hello World");
+    }
+
+    #[test]
+    fn extract_test_output_break_successes() {
+        let test_name = String::from("tests::test_1");
+        let lines = vec![
+            String::from("what"),
+            String::from("---- tests::test_1 stdout ----"),
+            String::from("Hello "),
+            String::from("World"),
+            String::from(""),
+            String::from("successes:"),
+        ];
+        assert_eq!(get_test_output(&test_name, &lines), "Hello World");
+    }
+
+    #[test]
+    fn extract_test_output_real() {
+        let test_name = String::from("tests::test_1");
+        let lines = vec![
+            "", 
+            "running 2 tests", 
+            "test tests::test_1 ... ok", 
+            "test tests::test_2 ... FAILED", 
+            "", 
+            "successes:", 
+            "", 
+            "---- tests::test_1 stdout ----", 
+            "Hello, Earthlings!", 
+            "", 
+            "", 
+            "successes:", 
+            "    tests::test_1", 
+            "", 
+            "failures:", 
+            "", 
+            "---- tests::test_2 stdout ----", 
+            "thread 'main' panicked at 'assertion failed: `(left == right)`", 
+            "  left: `1`,", " right: `2`', src/main.rs:14:9", 
+            "note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace", 
+            "", 
+            "", 
+            "failures:", 
+            "    tests::test_2", 
+            "", 
+            "test result: FAILED. 1 passed; 1 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.00s", 
+            "", 
+            ""
+        ];
+        let lines = lines.iter().map(|x| x.to_string()).collect();
+        assert_eq!(get_test_output(&test_name, &lines), "Hello, Earthlings!");
     }
 }
